7879f4c1fe5cbe4fb031cd90f18c97ffeaf3f27d
Unknown <Unknown@unknown.com>
2024-07-21 03:36:14 +0200

    Added Level Class with nothing in it for now

--- LunarExpansion/JVMClient/CheatMain/SDK/Minecraft.h
+++ LunarExpansion/JVMClient/CheatMain/SDK/Minecraft.h
@@ -4,16 +4,41 @@
 
 namespace SDK
 {
-	class CMinecraft {
+	namespace Offsets
+	{
+		namespace Minecraft
+		{
+
+		}
+	}
+
+	class CMinecraft 
+	{
 	private:
 		jobject Instance = nullptr;
 	public:
-        CMinecraft();
-        jobject GetInstance();
-        class CPlayer GetPlayer();
-        
-private:
-	jclass GetClass();
-};
+		CMinecraft();
+		jobject GetInstance();
+		class CPlayer GetPlayer();
+
+	private:
+		jclass GetClass();
+
+		class CClientLevel* level = nullptr;
+	};
+
+	class CLevel
+	{
+	private:
+		jobject* m_LevelReference = nullptr;
+
+		std::string LevelName = "";
+		int Dimension = 0;
+	};
+
+	class CClientLevel 
+	{
+
+	};
 }
 

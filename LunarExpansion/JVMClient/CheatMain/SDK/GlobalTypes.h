e0e70849a0c9633d701ba29dc3655bd5eb41c6f2
Unknown <Unknown@unknown.com>
2024-07-24 03:20:28 +0200

    Added Iterator Class in GlobalTypes.h for Iterating in c++ various Java Iterators. (Not finished yet tho)

--- LunarExpansion/JVMClient/CheatMain/SDK/GlobalTypes.h
+++ LunarExpansion/JVMClient/CheatMain/SDK/GlobalTypes.h
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <iostream>
 
+#include "../../Dependencies/Java.h"
+
+//Normal c++ types for different Calculations
 struct Vector2F {
 	float x, y;
 
@@ -16,6 +19,7 @@ struct Vector2F {
 	}
 };
 
+
 struct Vector3F {
 	float x, y, z;
 
@@ -28,3 +32,41 @@ struct Vector3F {
 		return std::sqrt(((other.x - this->x) * (other.x - this->x)) + ((other.y - this->y) * (other.y - this->y)) + ((other.z - this->z) * (other.z - this->z)));
 	}
 };
+
+
+//Java Types to C++
+
+
+namespace SDK
+{
+	namespace Offsets
+	{
+		namespace Iterator
+		{
+			bool InitializedIteratorOffsets = false;
+
+
+		}
+	}
+
+	template <typename T>
+	struct Iterator
+	{
+	public:
+		using Vector_T = std::vector<T>;
+
+		Vector_T GetVector();
+		void SetVector(Vector_T items);
+	private:
+		jobject m_OwningIteratorObject = nullptr;
+		Vector_T m_Items;
+
+		bool SetInteractionState(bool Locked);
+
+		//Get Data from Iterator to m_Items
+		void GetVectorData();
+		//Set Data from Iterator
+		void SetVectorData(Vector_T items)
+	};
+
+}
